137c6de7e154e8d1685fae8824c40fe652912d25
hanhead <hanhead@gmail.com>
2023-04-05 18:52:54 -0700

    completion test with console app

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,19 +1,28 @@
 ﻿using Newtonsoft.Json;
 using OpenAISharp;
+using OpenAISharp.API;
 
-string InitializationVectorBase64String = EncryptionUtils.GetNewInitailizationVectorBase64String();
-TripleDesEncryption tripleDesc = new TripleDesEncryption(EncryptionUtils.GetMacAddress16BytesFormat(), InitializationVectorBase64String);
+//CreateConfig();
+OpenAIConfiguration.Load();
+string _result = await Completions.Request("what is the best foods for a red wine?", Completions.Model.text_davinci_003, null, null, 1, 1);
+Console.WriteLine(_result);
+Console.ReadLine();
 
-var openAIConfiguration =new
+static void CreateConfig()
 {
-    OpenAI = new OpenAIConfiguration()
+    string InitializationVectorBase64String = EncryptionUtils.GetNewInitailizationVectorBase64String();
+    TripleDesEncryption tripleDesc = new TripleDesEncryption(EncryptionUtils.GetMacAddress16BytesFormat(), InitializationVectorBase64String);
+
+    var openAIConfiguration = new
     {
-        UrlPrefix = "https://api.openai.com/v1",
-        InitializationVectorBase64String = InitializationVectorBase64String,
-        EncryptedOrgID = tripleDesc.Encrypt("YOUR_ORGANIZATION_ID"),
-        EncryptedApiKey = tripleDesc.Encrypt("YOUR_API_KEY")
-    }
-};
-string _json = JsonConvert.SerializeObject(openAIConfiguration, Formatting.Indented);
-Console.WriteLine(_json);
-Console.ReadLine(); 
+        OpenAI = new OpenAIConfiguration()
+        {
+            UrlPrefix = "https://api.openai.com/v1",
+            InitializationVectorBase64String = InitializationVectorBase64String,
+            EncryptedOrgID = tripleDesc.Encrypt("YOUR_ORGANIZATION_ID"),
+            EncryptedApiKey = tripleDesc.Encrypt("YOUR_API_KEY")
+        }
+    };
+    string _json = JsonConvert.SerializeObject(openAIConfiguration, Formatting.Indented);
+    Console.WriteLine(_json);
+}
