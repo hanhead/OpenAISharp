48c0f65203f7ccdb046e6615816b1657117b1b73
hanhead <hanhead@gmail.com>
2023-04-07 15:16:14 -0700

    chatformat: initial commit

---
+++ OpenAISharp/API/chatformat.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAISharp.API
+{
+    public class chatformat
+    {
+        public enum roles
+        {
+            system,
+            user,
+            assistant
+        }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public roles role { get; set; }
+        public string content { get; set; }
+    }
+}
