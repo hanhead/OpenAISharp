5f1996760f6b73a90ac1f37c05be927971ffd19e
hanhead <hanhead@gmail.com>
2023-04-07 15:16:54 -0700

    chatresponse: initial commit

---
+++ OpenAISharp/API/ChatResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAISharp.API
+{
+    public class ChatResponse
+    {
+        public string id { get; set; }
+        public string @object { get; set; }
+        public int created { get; set; }
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+        public Error error { get; set; }
+
+    }
+}
