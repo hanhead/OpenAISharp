e55bbec31d66b8083f14e5f037f791d6754d0080
hanhead <hanhead@gmail.com>
2023-04-12 11:44:15 -0700

    add example code for new configuration

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,39 +1,49 @@
-﻿using Newtonsoft.Json;
-using OpenAISharp;
+﻿using OpenAISharp;
 using OpenAISharp.API;
 
 //CreateConfig();
-OpenAIConfiguration.Load();
+//await CompletionsExample();
+//await ChatExampleRaw();
+//await ChatExample()
 
-//CompletionsReponse _result = await Completions.Request("what is the best foods for a red wine?");
-//Console.WriteLine(_result.error != null ? _result.error.message : _result.choices[0].text);
-ChatResponse chatResponse = await Chat.Request(new Chat() { 
-    messages = new chatformat[] { 
-        new chatformat() {  role = chatformat.roles.system, content = "You are a pet behaviorist." },
-        new chatformat() {  role = chatformat.roles.user, content = "I have an aggressive German Shepherd who needs help managing its aggression." }
-    }
-});
-
-Console.WriteLine(chatResponse.error!=null?chatResponse.error.message:JsonConvert.SerializeObject(chatResponse.choices));
-
-
-Console.ReadLine();
 
 static void CreateConfig()
 {
-    string InitializationVectorBase64String = EncryptionUtils.GetNewInitailizationVectorBase64String();
-    TripleDesEncryption tripleDesc = new TripleDesEncryption(EncryptionUtils.GetMacAddress16BytesFormat(), InitializationVectorBase64String);
-
-    var openAIConfiguration = new
-    {
-        OpenAI = new OpenAIConfiguration()
-        {
-            UrlPrefix = "https://api.openai.com/v1",
-            InitializationVectorBase64String = InitializationVectorBase64String,
-            EncryptedOrgID = tripleDesc.Encrypt("YOUR_ORGANIZATION_ID"),
-            EncryptedApiKey = tripleDesc.Encrypt("YOUR_API_KEY")
-        }
-    };
-    string _json = JsonConvert.SerializeObject(openAIConfiguration, Formatting.Indented);
-    Console.WriteLine(_json);
+    Console.WriteLine("Input your Organization ID:");
+    string orgid = Console.ReadLine();
+    Console.WriteLine("Input your API Key:");
+    string apikey = Console.ReadLine();
+    OpenAIConfiguration.CreateConfigFile(orgid, apikey);
+    Console.WriteLine("appsettings.json file is created.");
+    Console.ReadLine();
 }
+
+static async Task CompletionsExample()
+{
+    OpenAIConfiguration.Load();
+    CompletionsReponse _result = await Completions.Request("what is the best foods for a red wine?");
+    Console.WriteLine(_result.error != null ? _result.error.message : _result.choices[0].text);
+}
+static async Task ChatExampleRaw()
+{
+    OpenAIConfiguration.Load();
+    ChatResponse chatResponse = await Chat.Request(new Chat()
+    {
+        messages = new chatformat[] {
+        new chatformat() {  role = chatformat.roles.system, content = "You are a pet behaviorist." },
+        new chatformat() {  role = chatformat.roles.user, content = "I have an aggressive German Shepherd who needs help managing its aggression." }
+    }
+    });
+
+    Console.WriteLine(chatResponse.error != null ? chatResponse.error.message : JsonConvert.SerializeObject(chatResponse.choices));
+    Console.ReadLine();
+}
+static async Task ChatExample()
+{
+    OpenAIConfiguration.Load();
+    string chatResponse = await Chat.Request("I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is \"I am in Los angeles and I want to visit only museums.\"");
+
+    Console.WriteLine(chatResponse);
+    Console.ReadLine();
+}
+
