0f7f2f5bec7bcb4c5eefb9060d386c115a183baa
hanhead <hanhead@gmail.com>
2023-04-13 13:25:05 -0700

    initial commit for moderations

--- OpenAISharp/API/Moderations.cs
+++ OpenAISharp/API/Moderations.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +15,53 @@ namespace OpenAISharp.API
         private static string command = "/moderations";
         public enum AvailableModel
         {
-            [Description("text-moderation-stable")]
+            [EnumMember(Value = "text-moderation-stable")]
             text_moderation_stable,
-            [Description("text-moderation-latest")]
+            [EnumMember(Value = "text-moderation-latest")]
             text_moderation_latest
+        }
+        public string input { get; set; }
+        public string model { get; set; }
+        
+        public static async Task<bool> isViolated(string input, AvailableModel? model = null)
+        {
+            ModerationsResponse response = await Request(input, model);
+            if (response.error == null)
+            {
+                return response.results.Count(r => r.flagged == true) > 0;
+            }
+            else
+            {
+                throw new Exception(response.error.message);
+            }
+        }
+        public static async Task<ModerationsResponse> Request(string input, AvailableModel? model = null)
+        {
+            ModerationsResponse moderationsResponse = null;
+            string result;
+            string organzationID = OpenAISettings.OrganizationID;
+            string apiKey = OpenAISettings.ApiKey;
+            var apiUrl = $"{OpenAISettings.UrlPrefix}{command}";
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+                client.DefaultRequestHeaders.Add("OpenAI-Organization", organzationID);
+                string requestJson = JsonConvert.SerializeObject(new Moderations 
+                { 
+                    input = input, 
+                    model = model==null?null:model.ToString()
+                }, Formatting.Indented, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+                StringContent requestJsonContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
+                using (HttpResponseMessage response = await client.PostAsync(apiUrl, requestJsonContent))
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                    moderationsResponse = JsonConvert.DeserializeObject<ModerationsResponse>(result);
+                }
+            }
+            return moderationsResponse;
         }
 
     }
---
+++ OpenAISharp/API/ModerationsResponse.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAISharp.API
+{
+    public class ModerationsResponse
+    {
+        public string id { get; set; }
+        public string model { get; set; }
+        public Error error { get; set; }
+        public List<Result> results { get; set; }
+
+        public class Categories
+        {
+            public bool hate { get; set; }
+
+            [JsonProperty("hate/threatening")]
+            public bool hatethreatening { get; set; }
+
+            [JsonProperty("self-harm")]
+            public bool selfharm { get; set; }
+            public bool sexual { get; set; }
+
+            [JsonProperty("sexual/minors")]
+            public bool sexualminors { get; set; }
+            public bool violence { get; set; }
+
+            [JsonProperty("violence/graphic")]
+            public bool violencegraphic { get; set; }
+        }
+
+        public class CategoryScores
+        {
+            public double hate { get; set; }
+
+            [JsonProperty("hate/threatening")]
+            public double hatethreatening { get; set; }
+
+            [JsonProperty("self-harm")]
+            public double selfharm { get; set; }
+            public double sexual { get; set; }
+
+            [JsonProperty("sexual/minors")]
+            public double sexualminors { get; set; }
+            public double violence { get; set; }
+
+            [JsonProperty("violence/graphic")]
+            public double violencegraphic { get; set; }
+        }
+
+        public class Result
+        {
+            public Categories categories { get; set; }
+            public CategoryScores category_scores { get; set; }
+            public bool flagged { get; set; }
+        }
+    }
+}
