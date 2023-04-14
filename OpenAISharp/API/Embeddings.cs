d955d9202d863de3a569255466508bf86f26b14f
hanhead <hanhead@gmail.com>
2023-04-13 19:46:48 -0700

    initial commit embeddings

--- OpenAISharp/API/Embeddings.cs
+++ OpenAISharp/API/Embeddings.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Text;
 
 namespace OpenAISharp.API
 {
@@ -12,6 +14,52 @@ namespace OpenAISharp.API
             [Description("text-search-ada-doc-001")]
             text_search_ada_doc_001
         }
-
+        [JsonIgnore]
+        public AvailableModel SelectedModel { get; set; }
+        private string _model;
+        public string model
+        {
+            get
+            {
+                if (SelectedModel != null)
+                {
+                    return SelectedModel.GetDescription();
+                }
+                else
+                {
+                    return _model;
+                }
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public string[] input { get; set; }
+        public string user { get; set; }
+        public static async Task<EmbeddingsResponse> Request(Embeddings embeddings)
+        {
+            EmbeddingsResponse embeddingsResponse = null;
+            string result;
+            string organzationID = OpenAISettings.OrganizationID;
+            string apiKey = OpenAISettings.ApiKey;
+            var apiUrl = $"{OpenAISettings.UrlPrefix}{command}";
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+                client.DefaultRequestHeaders.Add("OpenAI-Organization", organzationID);
+                string requestJson = JsonConvert.SerializeObject(embeddings, Formatting.Indented, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+                StringContent requestJsonContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
+                using (HttpResponseMessage response = await client.PostAsync(apiUrl, requestJsonContent))
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                    embeddingsResponse = JsonConvert.DeserializeObject<EmbeddingsResponse>(result);
+                }
+            }
+            return embeddingsResponse;
+        }
     }
 }
---
+++ OpenAISharp/API/EmbeddingsResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAISharp.API
+{
+    public class EmbeddingsResponse
+    {
+        public string @object { get; set; }
+        public List<Datum> data { get; set; }
+        public string model { get; set; }
+        public Usage usage { get; set; }
+        public class Datum
+        {
+            public string @object { get; set; }
+            public List<double> embedding { get; set; }
+            public int index { get; set; }
+            public Error error { get; set; }
+        }
+    }
+}
